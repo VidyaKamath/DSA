53c0c37af2c44a0609d67ec7c09f0af248fdbe6d
vidyakamath <pvidyakamath@gmail.com>
2022-04-14 21:25:16 -0500

    Solution to search inseert position

---
+++ C++/Easy/35. Search Insert Position.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+public:
+    int searchInsert(vector<int>& nums, int target)
+    {
+
+        int low = 0;
+        int high = nums.size() - 1;
+        int mid = 0;
+
+        while(low <= high)
+        {
+            // To avoid integer overflow bug
+            mid = low + (high - low) / 2;
+
+            // Binary Search
+            if(nums[mid] == target)
+                return mid;
+
+            else if(target < nums[mid])
+            {
+                high = mid - 1;
+            }
+            else if(target > nums[mid])
+            {
+                low = mid + 1;
+            }
+        }
+        return low;
+
+    }
+};
