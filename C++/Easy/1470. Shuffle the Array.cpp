e1e4a5698e303e8902cc1669ca4493dd883462eb
vidyakamath <pvidyakamath@gmail.com>
2022-04-14 21:38:48 -0500

    format source code

--- C++/Easy/1470. Shuffle the Array.cpp
+++ C++/Easy/1470. Shuffle the Array.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-    vector<int> shuffle(vector<int>& nums, int n) {
+    vector<int> shuffle(vector<int>& nums, int n)
+    {
         vector<int> result;
-        for(unsigned int i=0; i<n; i++)
+        for(unsigned int i = 0; i < n; i++)
         {
             result.push_back(nums[i]);
-            result.push_back(nums[n+i]);
+            result.push_back(nums[n + i]);
         }
-     return result;
+        return result;
     }
 };
--- C++/Easy/1480. Running Sum of 1d Array.cpp
+++ C++/Easy/1480. Running Sum of 1d Array.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
-    vector<int> runningSum(vector<int>& nums) {
-
-    unsigned int length = nums.size();
-    vector<int> sum(length, 0);
-
-    sum[0] = nums[0];
-    for(unsigned int idx=1; idx<length; idx++)
+    vector<int> runningSum(vector<int>& nums)
     {
-        sum[idx] = sum[idx-1] + nums[idx];
-    }
+        unsigned int length = nums.size();
+        vector<int> sum(length, 0);
 
-     return sum;
+        sum[0] = nums[0];
+        for(unsigned int idx = 1; idx < length; idx++)
+        {
+            sum[idx] = sum[idx - 1] + nums[idx];
+        }
+        return sum;
     }
 };
