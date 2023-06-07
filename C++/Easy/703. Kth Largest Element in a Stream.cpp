4c59320ee5741069325f520505b271911c2974ee
Vidya Kamath P <42544705+VidyaKamath@users.noreply.github.com>
2023-06-07 10:36:46 +0530

    Create 703. Kth Largest Element in a Stream.cpp

---
+++ C++/Easy/703. Kth Largest Element in a Stream.cpp
@@ -0,0 +1,30 @@
+class KthLargest {
+public:
+    KthLargest(int k, vector<int>& nums) {
+        this->k=k;
+        for(int i=0; i<nums.size(); i++){
+            min_heap.push(nums[i]);
+            if(min_heap.size()>k){
+                min_heap.pop();
+            }
+        }
+    }
+    
+    int add(int val) {
+        min_heap.push(val);
+        if(min_heap.size()>k){
+            min_heap.pop();
+        }
+        return min_heap.top();
+        
+    }
+private:
+    int k;
+    priority_queue<int, vector<int>, greater<int>> min_heap;
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
